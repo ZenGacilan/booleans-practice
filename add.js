019f2e224d89e2e2db53edcc6b0691bb2e1c7fb0
ZenGacilan <potapetekau@gmail.com>
2024-09-08 08:15:25 +0800

    initial commit

---
+++ add.js
@@ -0,0 +1,15 @@
+
+let hasPermission = false;
+
+let canEnter = isAdult && hasPermission;
+
+console.log(canEnter);
+
+
+let obj = {};
+
+let arr = [];
+
+console.log(Boolean(obj));
+
+console.log(Boolean(arr));
